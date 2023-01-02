16d79a4caaf58fc529b5866bbfc10cf11f0bc476
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-01-02 13:33:37 +0100

    Chore: testMintFromMintlistBeforeMintingStarts

--- test/ArtGobblers.t.sol
+++ test/ArtGobblers.t.sol
@@ -1,2 +1,106 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import {Utilities} from "./utils/Utilities.sol";
+import {console} from "./utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {stdError} from "forge-std/Test.sol";
+import {ArtGobblers, FixedPointMathLib} from "../src/ArtGobblers.sol";
+import {Goo} from "../src/Goo.sol";
+import {Pages} from "../src/Pages.sol";
+import {GooblerReserve} from "../src/utils/GobblerReserve.sol";
+import {RandProvider} from "../src/utils/rand/RandProvider.sol";
+import {ChainlinkV1RandProvider} from "../src/utils/rand/ChainlinkV1RandProvider.sol";
+import {LinkToken} from "./utils/mocks/LinkToken.sol";
+import {VRFCoordinatorMock} from "chainlink/v0.8/mocks/VRFCoordinatorMock.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {MockERC1155} from "solmate/test/utils/mocks/MockERC1155.sol";
+import {LibString} from "solmate/utils/LibString.sol";
+import {fromDaysWadUnsafe} from "solmate/utils/SignedWadMath.sol";
+
+/// @notice Unit test for Art Gobbler Contract.
+contract ArtGobblers is DSTestPlus {
+    using LibString for uint256;
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+
+    Utilities internal utils;
+    address payable[] internal users;
+
+    ArtGobblers internal gobblers;
+    VRFCoordinatorMock internal vrfCoordinator;
+    LinkToken internal linkToken;
+    Goo internal goo;
+    Pages internal pages;
+    GobblerReserve internal team;
+    GobblerReserve internal community;
+    RandProvider internal randProvider;
+
+    bytes32 private keyHash;
+    uint256 private fee;
+
+    uint256[] ids;
+
+    ////////////// SETUP //////////////
+
+    function setUp() public {
+        utils = new Utilities();
+        users = new utils.createUsers(5);
+        linkToken = new LinkToken();
+        vrfCoordinator = new VRFCoordinatorMock(address(linkToken));
+
+        // Gobblers contract will be deployed after 4 contract deploys, and pages after 5.
+        address gobblerAddress = utils.predictContractAddress(address(this), 4);
+        address pagesAddress = utils.predictContractAddress(address(this), 5);
+
+        team = new GobblerReserve(ArtGobblers(gobblerAddress), address(this));
+        community = new GooblerReserve(ArtGobblers(gobblerAddress), address(this));
+        randProvider = new ChainlinkV1RandProvider(
+            ArtGobblers(gobblerAddress),
+            address(vrfCoordinator),
+            address(linkToken),
+            keyHash,
+            fee
+        );
+
+        goo = new Goo(
+            //gobblers
+            utils.predictContractAddress(address(this), 1),
+            //Pages
+            utils.predictContractAddress(address(this), 2),
+        );
+
+        gobblers = new ArtGobblers(
+            keccak256(abi.encodePacked(users[0])),
+            block.timestamp,
+            goo,
+            Pages(pagesAddress),
+            address(team),
+            address(community),
+            randProvider,
+            "base",
+            "",
+            keccak256(abi.encodePacked("provenance")),
+        );
+
+        pages = new Pages(
+            block.timestamp, 
+            goo, 
+            address(0xBEEF),
+            gobblers,
+            "",
+        );
+    }
+
+    ///////// MINT TESTS ///////////
+
+    /// @notice Test that minting from the mintlist before minting starts fails.
+    function testMintFromMintlistBeforeMintingStarts() public {
+        vm.warp(block.timestamp - 1);
+        address user = users[0];
+        bytes32[] memory proof;
+        vm.prank(user);
+        vm.expectRevert(ArtGobblers.MintStartPending.selector);
+        gobblers.claimGobblers(proof);
+    }
+}
