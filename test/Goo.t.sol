5c91bd1b4d5aeeee38090db0559c0488a435b890
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-01-27 19:51:29 +0100

    test: more GooTest

--- test/Goo.t.sol
+++ test/Goo.t.sol
@@ -33,4 +33,32 @@ contract GooTest is DSTest {
         vm.expectRevert(Goo.Unathorized.selector);
         goo.mintForGobblers(address(this), mintAmount);
     }
+
+    function testSetPages() public {
+        goo.mintForGobblers(address(this), 1000000);
+        uint256 initialSupply = goo.totalSupply();
+        uint256 burnAmount = 1000000;
+        vm.prank(users[0]);
+        goo.burnForPages(address(this), burnAmount);
+        uint256 finalSupply = goo.totalSupply();
+        assertEq(finalSupply, initialSupply - burnAmount);
+    }
+
+    function testBurnAllowed() public {
+        uint256 mintAmount = 1000000;
+        goo.mintForGobblers(address(this), mintAmount);
+        uint256 burnAmount = 300000;
+        goo.burnForGobblers(address(this), burnAmount);
+        uint256 finalBalance = goo.balanceOf(address(this));
+        assertEq(finalBalance, mintAmount - burnAmount);
+    }
+
+    function testBurnNotAllowed() public {
+        uint256 mintAmount = 100000;
+        goo.mintForGobblers(address(this), mintAmount);
+        uint256 burnAmount = 200000;
+        vm.expectRevert(stdError.arithmeticError);
+        goo.burnForGobblers(address(this), burnAmount);
+
+    }
 }
